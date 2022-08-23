a02c1f76444657c1a07288411adea2d231d48ac6
ohwowoAo <sbjdnm@gmail.com>
2022-08-23 11:42:32 +0900

    feat: To Do List Delete button

--- todo-list/src/TodoList.js
+++ todo-list/src/TodoList.js
@@ -1,30 +1,54 @@
-import React, { useState } from 'react';
-import './TodoList.module.css'
+import { template } from 'lodash';
+import React, { useCallback, useState } from 'react';
+import style from './TodoList.module.css'
 
 const TodoList = () => {
     const [value, setValue] = useState('');
     const [id, setId] = useState(0);
+    const [finish, setFinish] = useState(false);
     const [todo, setTodo] = useState([]);
-
+    
     function submit(e){
         e.preventDefault();
-        const todolist = todo.concat({
-            id : id,
-            text: value
-        })
-        setId((current) => current + 1);
+        if(value === ''){
+            alert('Please enter to-do list ╰(*°▽°*)╯');
+        }else{
+            const todolist = todo.concat({
+                id : id,
+                text: value
+            })
+            setId((current) => current + 1);
+            
+            setValue('');
+            setTodo(todolist)
+        }
         
-        setTodo(todolist)
-        console.log(todo)
     }
-
+    const onRemove = (id) => {
+        const about_lists = todo.filter((list) => list.id !== id);
+        setTodo(about_lists);
+        console.log(id)
+    };
+    const onToggle = (id) => {
+        const finish_lists = todo.filter((list) => list.id === id);
+        setFinish(finish_lists);
+        setFinish(!finish)
+    }
+    function toggleDone() {
+        setFinish(!finish); 
+    }
+    // function onName(){
+    //     style.line
+    // }
+    // https://yeomss.tistory.com/205
+    // https://eunhee-programming.tistory.com/194
     const inputlist = todo.map( (list) => (
-        <li 
-        key={list.id}>
-            {list.text}
+        <li key={list.id} >
+            <span className={ finish ? style.line : ""}  onClick={ () =>onToggle(list.id)}>{list.text}</span>
+            <button onClick={() => onRemove(list.id)}>X</button>
         </li>
     ))
-
+    
     
     return (
         <div>
@@ -33,7 +57,7 @@ const TodoList = () => {
                 <input type="text" value={value} onChange={(e)=> setValue(e.target.value)} />
                 <button type='submit'>+</button>
             </form>
-            <ul className='todolist'>{inputlist}</ul>
+            <ul className={style.todolist}>{inputlist}</ul>
         </div>
     );
 };
